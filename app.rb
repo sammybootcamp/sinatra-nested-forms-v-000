0f452e6c3367dfb6f3a11a5167735aaa546f29bb
Sammy Wu <swucode@gmail.com>
2019-04-19 05:16:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,40 @@
 require './environment'
 
+class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
+end
+
+class Ship
+  attr_reader :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+end
+
 module FormsLab
   class App < Sinatra::Base
 
